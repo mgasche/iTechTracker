50e7ebd078cbdb09e4a07a6bb3ca449f00d86f98
Martial Gasche <martial.gasche@icloud.com>
2024-04-10 20:55:45 +0200

    Delete footer.js

--- javascript/footer.js
+++
@@ -1,9 +0,0 @@
-// JavaScript, um den Footer am unteren Rand zu positionieren
-window.addEventListener('DOMContentLoaded', (event) => {
-    const contentHeight = document.querySelector('.content').offsetHeight;
-    const windowHeight = window.innerHeight;
-
-    if (contentHeight < windowHeight) {
-        document.querySelector('.footer').classList.add('fixed-bottom');
-    }
-});
